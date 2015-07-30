227ccf9f59d60e41b25a8b17920fa1dc6b5aab94
Shannon Brady <shannonbrady2014@u.northwestern.edu>
2015-07-30 13:40:50 -0500

    updated cross object naming for template

--- Forward Search Template.R
+++ Forward Search Template.R
@@ -12,20 +12,22 @@ cross <- N2xCB4856cross
 #insert pheno data
 pheno <- readRDS("~/Dropbox/AndersenLab/LabFolders/PastMembers/Tyler/ForTrip/RIAILs2_processed.rds")
 
+#create completed cross object with pheno data set
+mapcross <- mergepheno(cross, pheno, set = 2)
+
 #map from subset of phenotype or load map object if mapping has been saved
 ## only mapping bleomycin from the RIAILs2 data set (reprocessed)
-#map <- fsearch(cross, iterations = 1000, phenotype="bleomycin")
+map <- fsearch(mapcross, iterations = 1000, phenotype="bleomycin")
 
-#save(map, file="~/Dropbox/AndersenLab/LabFolders/Shannon/Scripts/Linkage Mapping/forwardsearchbleo.Rda")
+save(map, file="~/Dropbox/AndersenLab/LabFolders/Shannon/Scripts/Linkage Mapping/forwardsearchbleo.Rda")
 
 #this mapping was already done, loading the mapping info now under name "map"
-load("~/Dropbox/AndersenLab/LabFolders/Shannon/Scripts/Linkage Mapping/forwardsearchbleo.Rda")
+#load("~/Dropbox/AndersenLab/LabFolders/Shannon/Scripts/Linkage Mapping/forwardsearchbleo.Rda")
 
-#create completed cross object with pheno data set
-cross <- mergepheno(cross, pheno, set = 2)
+
 
 #annotate lods to find CI, VE, ES
-annotatedlods <- annotate_lods(map, cross)
+annotatedlods <- annotate_lods(map, mapcross)
 
 #subset your trait of interest
 mean.TOF <- filter(annotatedlods, trait=="bleomycin.mean.TOF")
@@ -34,7 +36,7 @@ mean.TOF <- filter(annotatedlods, trait=="bleomycin.mean.TOF")
 lodplot(mean.TOF)
 
 #plot the effect size
-effectplot(cross, mean.TOF)
+effectplot(mapcross, mean.TOF)
 
 #plot the phenotype by genotype split
-pxgplot(cross, mean.TOF)
+pxgplot(mapcross, mean.TOF)
